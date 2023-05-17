5936c7b2c3788347b1e109159ea8882f8dea9553
Alekseeva Elena <elena_itt@mail.ru>
2023-05-17 11:54:23 +0300

    add external cycle

--- ugadaika_slov.py
+++ ugadaika_slov.py
@@ -82,32 +82,39 @@ def display_hangman(tries):
     return stages[tries]
 
 #def play():   # функция процесса игры
-
-tries = 6  # присваиваем начальное значение виселицы
 print('Давайте играть в угадайку слов!')
-word = get_word()
-w = len(word) * '_'  # создаем строку загаданного слова
-w = list(w)  # переводим строку загаданного слова в список
-print(''.join(w))  # вывод длины загаданного слова
-print(display_hangman(tries)) # Вывод начальной виселицы
+while True:
+    tries = 6  # присваиваем начальное значение виселицы
+    word = get_word()
+    w = len(word) * '_'  # создаем строку загаданного слова
+    w = list(w)  # переводим строку загаданного слова в список
+    print(''.join(w))  # вывод длины загаданного слова
+    print(display_hangman(tries)) # Вывод начальной виселицы
 
-while tries > 0:
-    print("Введите букву или слово")
-    litera = input().upper()
-    if litera in word and len(litera) == 1: # провверка наличия введенной БУКВЫ в слове
-        for i in range(len(word)):
-            if word[i] == litera:
-                w[i] = litera
-                print(''.join(w))
-                print(display_hangman(tries))
-    elif litera == word:  # проверка введенного СЛОВА
-        print("Ура! Вы победили!")
+    while tries > 0:
+        print("Введите букву или слово")
+        litera = input().upper()
+        if litera in word and len(litera) == 1: # провверка наличия введенной БУКВЫ в слове
+            for i in range(len(word)):
+                if word[i] == litera:
+                    w[i] = litera
+                    print(''.join(w))
+                    print(display_hangman(tries))
+        elif litera == word:  # проверка введенного СЛОВА
+            print("Ура! Вы победили!")
+            break
+        else:  # действия в случае если ни буква, ни слово не угаданы
+            tries -= 1
+            print(''.join(w))
+            print("Упс! Ошибочка..")
+            print(display_hangman(tries))
+    print("Если хотите играть снова введите +")
+    new_game = input()
+    if new_game == "+":
+        continue
+    else:
+        print("Приходите поиграть еще!")
         break
-    else:  # действия в случае если ни буква, ни слово не угаданы
-        tries -= 1
-        print(''.join(w))
-        print("Упс! Ошибочка..")
-        print(display_hangman(tries))
-print("Попробовать играть снова?")
+
 
 
